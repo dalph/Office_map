e9e0b45e36f9388a02c2ffd1d05e4ba59881dc5a
iparinile <poselennov.il@ya.ru>
2019-11-12 19:20:42 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import UsersComp from "./UsersComp";
+import MapComp from "./MapComp";
+
+
+let dict = {
+    1: 1,
+    2: 2,
+    3: 3,
+    4: null,
+    5: null,
+};
+
+
+class App extends React.Component {
+    state = {
+        placeId: null
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Здесь будет какая-то шапка {this.state.placeId}
+                    </p>
+                </header>
+                <div className="body">
+                    <MapComp onPlace={(e) => {
+                        this.setState({placeId: e.target.dataset.placeid})
+                        console.log(e.target.dataset.placeid);
+                    }
+                    }/>
+                    <UsersComp selectedPlace={dict[this.state.placeId]}/>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default App;
